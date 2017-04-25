6597f8675ae79ca15180400cfc3d27773769a807
foxrc <ryanfox@Ryans-iMac.home>
2017-04-25 01:32:02 -0400

    Finished insert to medication
    Finished insert to medication

--- Animal_Care_System/insert_medication.php
+++ Animal_Care_System/insert_medication.php
@@ -1,6 +1,32 @@
 <?php
 require 'connection.php';
 session_start();
+$visit_id = (int) $_SESSION['visit_id'];
+$pro_id = (int) $_SESSION['pro_id'];
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    if (isset($_POST['drug'])) {
+        //if drug id isset
+        $drugid = (int)$_POST['drug'];
+    }
+    if (isset($_POST['quantity'])) {
+        //if quantity is set
+        $quantity = (int)$_POST['quantity'];
+    }
+    if (empty($_POST['instructions'])) {
+        //if instructions are empty set an error message
+        $err_msg = "Must enter in instructions for medication";
+    } else {
+
+        $instructions = $_POST['instructions'];
+    }
+    if (insert_medication($drugid, $quantity, $instructions, $pro_id, $visit_id) === "SUCCESS") {
+        header("Location:view_medications.php");
+    }
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -32,18 +58,18 @@ require 'is_employee.php';
 <div class="form_layout">
     <form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <h2>Insert Medication</h2>
-    <select>
+    <select name = 'drug'>
         <?php
             $drug = get_drug();
             while($row = mysqli_fetch_assoc($drug)){
-                echo '<option value =' .$row['DrugId'] . ">" . $row['name'] ." " . $row['dosage'] ."</option>";
+                echo '<option value =' . $row['DrugId'] . ">" . $row['name'] ." " . $row['dosage'] ."</option>";
             }
         ?>
     </select>
         <br>
     <script>
         //adds select tag to page and loops from numbers 0-100 for quantity
-        $("<select id ='quantity'> </select>").appendTo('form');
+        $("<select name = 'quantity'  id ='quantity'> </select>").appendTo('form');
         for(var i = 0; i<=100; i++){
 
             $("#quantity").append("<option value =" + i +">" + i + "</option>");
@@ -51,7 +77,7 @@ require 'is_employee.php';
     </script>
         <br>
 
-    <textarea rows = "6" cols="50">
+    <textarea name = 'instructions'rows = "6" cols="50">
       Instructions for medication..
 
     </textarea><br/>
@@ -81,8 +107,19 @@ require 'is_employee.php';
 
     }
 
-    function insert_medication(){
+    function insert_medication($drugid,$quantity,$instructions,$pro_id,$visit_id){
+
+        $query = "INSERT INTO medication(DrugId,Quantity,Instructions,ProcedureId,VisitId) VALUES ($drugid,$quantity,'$instructions',$pro_id,$visit_id)";
+
+        if(mysqli_query($GLOBALS['conn'],$query)){
+
+            return "SUCCESS";
+        }else{
+
+            return "FAILURE";
+        }
 
 
     }
+
 ?>
