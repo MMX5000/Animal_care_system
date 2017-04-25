f4131fa61d2a18de1461eac516c7c90df04c7dbb
decojl <decordova.jl@gmail.com>
2017-04-25 09:59:09 -0400

    Fixed homepage

--- Animal_Care_System/employee_home.php
+++ Animal_Care_System/employee_home.php
@@ -32,7 +32,6 @@ and open the template in the editor.
         </div>
         <h2>Today's Appointments <?php echo $currentDate ?>: </h2>
         <div class ="appointment_div">
-
             <?php appointment_by_date($currentDate);?>
 
         </div>
